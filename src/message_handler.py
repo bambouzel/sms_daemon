8eff757af17efa3f6ff701f1e3aa7e53015dcd1c
bambouzel <ivo.abeloos@gmail.com>
2019-10-20 18:52:10 +0200

    oops

--- src/message_handler.py
+++ src/message_handler.py
@@ -9,7 +9,8 @@ import time
 #
 # constants in module
 #
-URL = "http://192.168.1.38:8123/api/states"
+URL_STATES = "http://192.168.1.38:8123/api/states"
+URL_SERVICES = "http://192.168.1.38:8123/api/services"
 BEARER = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI4YTNkZDllNjIzM2E0YTU3YjNlOGNlNzc0OTZmZmE1NCIsImlhdCI6MTU3MDI5Mzk5MSwiZXhwIjoxODg1NjUzOTkxfQ.r7NSM-mCnOmsBS3WHO90DIBi2JCyAJuggC5zTP2a2hg"
 HEADERS = {
     'Authorization': 'Bearer ' + BEARER,
@@ -34,16 +35,16 @@ class MessageHandler:
             sms.sendSMS(recipient, self.arm())
 
     def status(self):
-        response = get(URL + '/alarm_control_panel.home_alarm', headers=HEADERS)
+        response = get(URL_STATES + '/alarm_control_panel.home_alarm', headers=HEADERS)
         data = json.loads(response.text)
         return data['state']
 
     def arm(self):
-        response = post(URL + '/alarm_control_panel/alarm_arm_home', headers=HEADERS, json=JSON)
+        response = post(URL_SERVICES + '/alarm_control_panel/alarm_arm_home', headers=HEADERS, json=JSON)
         data = json.loads(response.text)
         return data['state']
 
     def disarm(self):
-        response = post(URL + '/alarm_control_panel/alarm_disarm', headers=HEADERS, json=JSON)
+        response = post(URL_SERVICES + '/alarm_control_panel/alarm_disarm', headers=HEADERS, json=JSON)
         data = json.loads(response.text)
         return data['state']
